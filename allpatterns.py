f699a25c74c4618a2fed0d1c4b91264d94f0e6b1
katinkab <katinka.becker@fu-berlin.de>
2018-04-27 12:21:00 +0200

    script to generate all patterns

---
+++ allpatterns.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+def getallpatterns(FnameASP,
+	           DegLowerBound,
+		   DegUpperBound):
+	print("Hello.")
+
+
+
+
+getallpatterns("2018/AnswerSetLAD/data/10x10input.asp",0,10)
