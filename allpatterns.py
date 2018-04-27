01fbafb4dc70bd1b708032b0de7d7f915af87268
katinkab <katinka.becker@fu-berlin.de>
2018-04-27 12:32:34 +0200

    script to generate all patterns

--- allpatterns.py
+++ allpatterns.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
 
-def getallpatterns(FnameASP,
+def getallpatterns(FnameDATA,
+		   FnamePAT,
 	           DegLowerBound,
 		   DegUpperBound):
-	print("Hello.")
+
+        print "--- getallpatterns"
+        print " data file:", FnameDATA
+	print " patterns calculated with:", FnamePAT
+
+	pat_file= [""]
+    	pat_file+= ["%% All patterns generated with %s from %s from degree %i to %i"%(FnamePAT,FnameDATA,DegLowerBound,DegUpperBound)]
+	pat_file+= [""]
+
+		
 
 
 
 
-getallpatterns("2018/AnswerSetLAD/data/10x10input.asp",0,10)
+getallpatterns("2018/AnswerSetLAD/data/10x10input.asp","2018/AnswerSetLAD/AnswerSetLAD_primepattern.asp",0,10)
