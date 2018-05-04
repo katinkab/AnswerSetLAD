b71bd091121e2e8c6af06c35b74053063c6a740c
katinkab <katinka.becker@fu-berlin.de>
2018-05-04 13:07:35 +0200

    get all patterns

--- allpatterns.py
+++ allpatterns.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import subprocess
+
 def getallpatterns(FnameDATA,
 		   FnamePAT,
 	           DegLowerBound,
@@ -8,14 +10,20 @@ def getallpatterns(FnameDATA,
         print "--- getallpatterns"
         print " data file:", FnameDATA
 	print " patterns calculated with:", FnamePAT
+	print " lower bound degree:", DegLowerBound
+	print " upper bound degree:", DegUpperBound
+	
+	low = int(DegLowerBound)
+	high = int(DegUpperBound)
 
-	pat_file= [""]
-    	pat_file+= ["%% All patterns generated with %s from %s from degree %i to %i"%(FnamePAT,FnameDATA,DegLowerBound,DegUpperBound)]
-	pat_file+= [""]
-
+	for deg in range(low,high+1):
+		process = subprocess.Popen(["Arbeitsfläche/clingo-4.5.4-linux-x86_64/clingo", "2018/KaLAD/data/SPECT_test.kalad", "2018/AnswerSetLAD/AnswerSetLAD_primepattern.asp", "-c sign=0", "-c degree=%i"%deg, "-c homogeneity=100", "-c prevalence=0", "--quiet", "-n 0"], stdout=subprocess.PIPE)
+		while True:
+  			line = process.stdout.readline()
+  			if line !="":
+				if "Models" in line:
+					print "degree %i:"%deg, line.rstrip()
+  			else:
+    				break
 		
-
-
-
-
-getallpatterns("2018/AnswerSetLAD/data/10x10input.asp","2018/AnswerSetLAD/AnswerSetLAD_primepattern.asp",0,10)
+getallpatterns("2018/AnswerSetLAD/data/10x10input.asp","2018/AnswerSetLAD/AnswerSetLAD_primepattern.asp",1,5)
