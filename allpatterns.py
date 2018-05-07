f61f4c0909a54c7f6c370c4433e7f9cbfe2742cf
katinkab <katinka.becker@fu-berlin.de>
2018-05-07 11:43:33 +0200

    all patterns

--- allpatterns.py
+++ allpatterns.py
@@ -5,25 +5,53 @@ import subprocess
 def getallpatterns(FnameDATA,
 		   FnamePAT,
 	           DegLowerBound,
-		   DegUpperBound):
+		   DegUpperBound,
+		   Hom,
+		   Prev):
 
-        print "--- getallpatterns"
+        print "--- getallpatterns starts calculating..."
         print " data file:", FnameDATA
 	print " patterns calculated with:", FnamePAT
 	print " lower bound degree:", DegLowerBound
 	print " upper bound degree:", DegUpperBound
+	print ""
 	
 	low = int(DegLowerBound)
 	high = int(DegUpperBound)
+		
+	clingo = "Arbeitsfläche/clingo-4.5.4-linux-x86_64/clingo"
+
+	print " positive patterns:"
 
 	for deg in range(low,high+1):
-		process = subprocess.Popen(["Arbeitsfläche/clingo-4.5.4-linux-x86_64/clingo", "2018/KaLAD/data/SPECT_test.kalad", "2018/AnswerSetLAD/AnswerSetLAD_primepattern.asp", "-c sign=0", "-c degree=%i"%deg, "-c homogeneity=100", "-c prevalence=0", "--quiet", "-n 0"], stdout=subprocess.PIPE)
+		process = subprocess.Popen([clingo, FnameDATA, FnamePAT, "-c sign=1", "-c degree=%i"%deg, "-c homogeneity=%i"%Hom, "-c prevalence=%i"%Prev, "-n 0"], stdout=subprocess.PIPE)
 		while True:
   			line = process.stdout.readline()
   			if line !="":
 				if "Models" in line:
-					print "degree %i:"%deg, line.rstrip()
+					print "  degree %i:"%deg, line.rstrip()
+				elif "pat" in line:
+					print " ", line.rstrip()
   			else:
+				print ""
     				break
+
+	print ""
+	print " negative patterns:"
+
+	for deg in range(low,high+1):
+		process = subprocess.Popen([clingo, FnameDATA, FnamePAT, "-c sign=0", "-c degree=%i"%deg, "-c homogeneity=%i"%Hom, "-c prevalence=%i"%Prev, "-n 0"], stdout=subprocess.PIPE)
+		while True:
+  			line = process.stdout.readline()
+  			if line !="":
+				if "Models" in line:
+					print "  degree %i:"%deg, line.rstrip()
+				elif "pat" in line:
+					print " ", line.rstrip()
+  			else:
+				print ""
+    				break
+
+        print "--- getallpatterns done. "
 		
-getallpatterns("2018/AnswerSetLAD/data/10x10input.asp","2018/AnswerSetLAD/AnswerSetLAD_primepattern.asp",1,5)
+getallpatterns("2018/AnswerSetLAD/data/10x10input.asp","2018/AnswerSetLAD/AnswerSetLAD_primepattern.asp", 1, 3, 80, 80)
