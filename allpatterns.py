9020e5517e421fd73e4c329683b0470612fd4e41
katinkab <katinka.becker@fu-berlin.de>
2018-05-07 12:42:23 +0200

    allpatterns call from terminal

--- allpatterns.py
+++ allpatterns.py
@@ -1,30 +1,40 @@
 # -*- coding: utf-8 -*-
 
 import subprocess
+import sys
 
 def getallpatterns(FnameDATA,
 		   FnamePAT,
 	           DegLowerBound,
 		   DegUpperBound,
-		   Hom,
-		   Prev):
+		   Hom=100,
+		   Prev=0,
+		   *IsQuiet):
 
         print "--- getallpatterns starts calculating..."
         print " data file:", FnameDATA
 	print " patterns calculated with:", FnamePAT
 	print " lower bound degree:", DegLowerBound
 	print " upper bound degree:", DegUpperBound
+	print " homogeneity in percent (lower bound):", Hom
+	print " prevalence in percent (lower bound):", Prev
 	print ""
 	
 	low = int(DegLowerBound)
 	high = int(DegUpperBound)
+	
+	homint = int(Hom)
+	prevint = int(Prev)
 		
 	clingo = "Arbeitsfläche/clingo-4.5.4-linux-x86_64/clingo"
 
 	print " positive patterns:"
 
 	for deg in range(low,high+1):
-		process = subprocess.Popen([clingo, FnameDATA, FnamePAT, "-c sign=1", "-c degree=%i"%deg, "-c homogeneity=%i"%Hom, "-c prevalence=%i"%Prev, "-n 0"], stdout=subprocess.PIPE)
+		if "quiet" in IsQuiet:
+			process = subprocess.Popen([clingo, FnameDATA, FnamePAT, "-c sign=1", "-c degree=%i"%deg, "-c homogeneity=%i"%homint, "-c prevalence=%i"%prevint, "-n 0", "--quiet"], stdout=subprocess.PIPE)
+		else:
+			process = subprocess.Popen([clingo, FnameDATA, FnamePAT, "-c sign=1", "-c degree=%i"%deg, "-c homogeneity=%i"%homint, "-c prevalence=%i"%prevint, "-n 0"], stdout=subprocess.PIPE)
 		while True:
   			line = process.stdout.readline()
   			if line !="":
@@ -40,7 +50,10 @@ def getallpatterns(FnameDATA,
 	print " negative patterns:"
 
 	for deg in range(low,high+1):
-		process = subprocess.Popen([clingo, FnameDATA, FnamePAT, "-c sign=0", "-c degree=%i"%deg, "-c homogeneity=%i"%Hom, "-c prevalence=%i"%Prev, "-n 0"], stdout=subprocess.PIPE)
+		if "quiet" in IsQuiet:
+			process = subprocess.Popen([clingo, FnameDATA, FnamePAT, "-c sign=0", "-c degree=%i"%deg, "-c homogeneity=%i"%homint, "-c prevalence=%i"%prevint, "-n 0", "--quiet"], stdout=subprocess.PIPE)
+		else:
+			process = subprocess.Popen([clingo, FnameDATA, FnamePAT, "-c sign=0", "-c degree=%i"%deg, "-c homogeneity=%i"%homint, "-c prevalence=%i"%prevint, "-n 0"], stdout=subprocess.PIPE)
 		while True:
   			line = process.stdout.readline()
   			if line !="":
@@ -53,5 +66,6 @@ def getallpatterns(FnameDATA,
     				break
 
         print "--- getallpatterns done. "
-		
-getallpatterns("2018/AnswerSetLAD/data/10x10input.asp","2018/AnswerSetLAD/AnswerSetLAD_primepattern.asp", 1, 3, 80, 80)
+
+if __name__ == '__main__':
+    getallpatterns(*sys.argv[1:])
